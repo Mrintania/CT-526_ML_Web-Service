96e46dbbaed6adcf070158aed82557e3e41ce085
Mrintania <nui-nui-nui@windowslive.com>
2024-03-24 19:01:36 +0700

    fix from number to float

--- flask for web service/iris_predict.py
+++ flask for web service/iris_predict.py
@@ -14,7 +14,7 @@ def train_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
-    return model
+    return model    
 
 
 def predict_iris(features):
